d1ca0ae2cbfc49ba64ddf5baa9247344673141bf
Jan Duchscherer <duchsche@hm.edu>
2025-05-10 17:38:25 +0200

    feat: add configs to __init__

--- unitraj/__init__.py
+++ unitraj/__init__.py
@@ -1,6 +1,14 @@
+# from .configs.experiment_config import ExperimentConfig
 from .configs.experiment_config import ExperimentConfig
 from .configs.path_config import PathConfig
+from .configs.wandb_config import WandbConfig
 from .datasets.base_dataset import DatasetConfig
+from .datasets.dataparser import DataParserConfig
+from .lightning.lit_datamodule import LitDatamoduleConfig
+from .lightning.lit_trainer_factory import (
+    LitTrainerFactoryConfig,
+    TrainerCallbacksConfig,
+)
 from .models.autobot.autobot import AutoBotConfig
 
 __all__ = [
@@ -8,4 +16,12 @@ __all__ = [
     "DatasetConfig",
     "ExperimentConfig",
     "PathConfig",
+    "LitTrainerFactoryConfig",
+    "TrainerCallbacksConfig",
+    "WandbConfig",
+    "LitTrainerFactoryConfig",
+    "TrainerCallbacksConfig",
+    "WandbConfig",
+    "LitDatamoduleConfig",
+    "DataParserConfig",
 ]
